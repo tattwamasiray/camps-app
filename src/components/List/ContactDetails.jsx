56ac88c35d8d56b194b0404403f10a611daef810
Subhendu Swain <suvendu.swain@gmail.com>
2022-12-09 10:59:26 +1100

    Add direct call support

--- src/components/List/ContactDetails.jsx
+++ src/components/List/ContactDetails.jsx
@@ -4,7 +4,10 @@ const ContactDetails = ({contactDetails}) => {
     return (contactDetails ? <div>
         <ul className="list-unstyled mb-6">
             <li className="d-inline-block me-4 mb-2">
-                <i className="fas fa-phone-alt me-2" aria-hidden="true"></i>{contactDetails?.formatted_phone_number}
+                {contactDetails?.formatted_phone_number && (<><i className="fas fa-phone-alt me-2"
+                                                                 aria-hidden="true"></i>
+                    <a
+                        href={`tel:${contactDetails?.formatted_phone_number}`}>{contactDetails?.formatted_phone_number}</a></>)}
             </li>
             {contactDetails?.website && <a href={contactDetails?.website} target="_blank">web site</a>}
         </ul>
